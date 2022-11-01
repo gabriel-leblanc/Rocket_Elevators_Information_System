0823f6b704709be49bf27f8ea61f9cf1e6e08dd1
gabriel-leblanc <gabriel.leblanc@icloud.com>
2022-11-01 13:07:17 -0400

    redo customer final

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_01_154550) do
+ActiveRecord::Schema.define(version: 2022_11_01_165528) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "address_type"
