729dc55c2dd22bb6cd65b9b85d96da34965b49f1
lauranav <lauranavp@hotmail.com>
2022-11-02 13:04:24 -0400

    merge dev avec table elevators

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_02_145610) do
+ActiveRecord::Schema.define(version: 2022_11_02_145613) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3", force: :cascade do |t|
     t.string "address_type"
