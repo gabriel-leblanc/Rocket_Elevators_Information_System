085f43839a6219e195e63d20a88dd9964423f41f
gabriel-leblanc <gabriel.leblanc@icloud.com>
2022-11-03 12:04:14 -0400

    faker building fait

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,115 +11,114 @@ require "faker"
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # User Table 
-# user = User.create!(email: "mathieu.houde@codeboxx.biz", password: "Mathieu321!")
-# employee = Employee.create!(
-#     first_name: 'Mathieu',
-#     last_name: 'Houde',
-#     title: 'Gopher',
-#     email: 'mathieu.houde@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "mathieu.houde@codeboxx.biz", password: "Mathieu321!")
+employee = Employee.create!(
+    first_name: 'Mathieu',
+    last_name: 'Houde',
+    title: 'Gopher',
+    email: 'mathieu.houde@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "patrick.thibault@codeboxx.biz", password: "Patrick321!")
-# employee = Employee.create!(
-#     first_name: 'Patrick',
-#     last_name: 'Thibault',
-#     title: 'Maximalist',
-#     email: 'patrick.thibault@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "patrick.thibault@codeboxx.biz", password: "Patrick321!")
+employee = Employee.create!(
+    first_name: 'Patrick',
+    last_name: 'Thibault',
+    title: 'Maximalist',
+    email: 'patrick.thibault@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "francis.patry-jessop@codeboxx.biz", password: "Francis321!")
-# employee = Employee.create!(
-#     first_name: 'Francis',
-#     last_name: 'Patry-Jessop',
-#     title: 'Captain',
-#     email: 'francis.patry-jessop@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "francis.patry-jessop@codeboxx.biz", password: "Francis321!")
+employee = Employee.create!(
+    first_name: 'Francis',
+    last_name: 'Patry-Jessop',
+    title: 'Captain',
+    email: 'francis.patry-jessop@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "david.amyot@codeboxx.biz", password: "David321!")
-# employee = Employee.create!(
-#     first_name: 'David',
-#     last_name: 'Amyot',
-#     title: 'The Man',
-#     email: 'david.amyot@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "david.amyot@codeboxx.biz", password: "David321!")
+employee = Employee.create!(
+    first_name: 'David',
+    last_name: 'Amyot',
+    title: 'The Man',
+    email: 'david.amyot@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "marie-eve.goupil@codeboxx.biz", password: "Marie-eve321!")
-# employee = Employee.create!(
-#     first_name: 'Marie-Ève',
-#     last_name: 'Goupil',
-#     title: 'AI Master',
-#     email: 'marie-eve.goupil@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "marie-eve.goupil@codeboxx.biz", password: "Marie-eve321!")
+employee = Employee.create!(
+    first_name: 'Marie-Ève',
+    last_name: 'Goupil',
+    title: 'AI Master',
+    email: 'marie-eve.goupil@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "francois.boivin@codeboxx.biz", password: "Francois321!")
-# employee = Employee.create!(
-#     first_name: 'François',
-#     last_name: 'Boivin',
-#     title: 'The tank',
-#     email: 'francois.boivin@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "francois.boivin@codeboxx.biz", password: "Francois321!")
+employee = Employee.create!(
+    first_name: 'François',
+    last_name: 'Boivin',
+    title: 'The tank',
+    email: 'francois.boivin@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "timothy.wever@codeboxx.biz", password: "Timothy321!")
-# employee = Employee.create!(
-#     first_name: 'Timothy',
-#     last_name: 'Wever',
-#     title: 'Beard whisperer',
-#     email: 'timothy.wever@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "timothy.wever@codeboxx.biz", password: "Timothy321!")
+employee = Employee.create!(
+    first_name: 'Timothy',
+    last_name: 'Wever',
+    title: 'Beard whisperer',
+    email: 'timothy.wever@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "kiril.kleinerman@codeboxx.biz", password: "Kiril321!")
-# employee = Employee.create!(
-#     first_name: 'Kiril',
-#     last_name: 'Kleinerman',
-#     title: 'I <3 Winnipeg',
-#     email: 'kiril.kleinerman@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "kiril.kleinerman@codeboxx.biz", password: "Kiril321!")
+employee = Employee.create!(
+    first_name: 'Kiril',
+    last_name: 'Kleinerman',
+    title: 'I <3 Winnipeg',
+    email: 'kiril.kleinerman@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "felicia.hartono@codeboxx.biz", password: "Felicia321!")
-# employee = Employee.create!(
-#     first_name: 'Felicia',
-#     last_name: 'Hartono',
-#     title: 'Scrums are too early',
-#     email: 'felicia.hartono@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "felicia.hartono@codeboxx.biz", password: "Felicia321!")
+employee = Employee.create!(
+    first_name: 'Felicia',
+    last_name: 'Hartono',
+    title: 'Scrums are too early',
+    email: 'felicia.hartono@codeboxx.biz',
+    user: user
+)
 
-# user = User.create!(email: "eileen.ai@codeboxx.biz", password: "Eileen321!")
-# employee = Employee.create!(
-#     first_name: 'Eilen',
-#     last_name: 'Ai',
-#     title: 'They really are.',
-#     email: 'eileen.ai@codeboxx.biz',
-#     user: user
-# )
+user = User.create!(email: "eileen.ai@codeboxx.biz", password: "Eileen321!")
+employee = Employee.create!(
+    first_name: 'Eilen',
+    last_name: 'Ai',
+    title: 'They really are.',
+    email: 'eileen.ai@codeboxx.biz',
+    user: user
+)
 
-# # Address table  
+# Address table  
 
 file = File.open "address/addresses-us-1000.json"
 data = JSON.load file 
 
-puts "data: #{data}"
-# address = Address.create!(
-#     number_and_street:data['addresses'][1]['address1'],
-#     suite_appartment:data['addresses'][1]['address2'],
-#     city:data['addresses'][1]['city'],
-#     postal_code:data['addresses'][1]['postalCode'],
-#     country:"US"
+address = Address.create!(
+    number_and_street:data['addresses'][1]['address1'],
+    suite_appartment:data['addresses'][1]['address2'],
+    city:data['addresses'][1]['city'],
+    postal_code:data['addresses'][1]['postalCode'],
+    country:"US"
     
-# )
-# puts address.city
-# puts address.number_and_street
-# puts address.suite_appartment
-# puts address.postal_code
-# puts address.country
+)
+puts address.city
+puts address.number_and_street
+puts address.suite_appartment
+puts address.postal_code
+puts address.country
 
 
 #Faker
@@ -139,30 +138,15 @@ puts "data: #{data}"
 #     )
 # end
 
-<<<<<<< HEAD
 15.times do
     buildings = Building.create!(
             # customerID: ID,
-            # buildingAddress
-            adminFullName: Faker::FunnyName.name || Faker::Creature::Animal.name,
-            # adminEmail
-            # adminPhoneNumber
-            # technicalContactFullName
-            # technicalContactEmail
-            # technicalContactPhoneNumber
+            buildingAddress: Faker::Address.full_address,
+            adminFullName: Faker::FunnyName.name,
+            adminEmail: Faker::Internet.free_email,
+            adminPhoneNumber: Faker::PhoneNumber.cell_phone,
+            technicalContactFullName: Faker::Name.name_with_middle,
+            technicalContactEmail: Faker::Internet.free_email,
+            technicalContactPhoneNumber: Faker::PhoneNumber.cell_phone,
     )
 end
-=======
-# 5.times do
-#     buildings = Building.create!(
-#             customerID: ID,
-#             # buildingAddress
-#             adminFullName: Faker::FunnyName.name,
-#             # adminEmail
-#             # adminPhoneNumber
-#             # technicalContactFullName
-#             # technicalContactEmail
-#             # technicalContactPhoneNumber
-#     )
-# end
->>>>>>> dev
